717abb9ef42bfbd9023e0f488dc95cc69958332d
Mao <maoakiba@gmail.com>
2020-02-01 18:03:21 +0100

    Particleeeeeeeeeees

---
+++ Assets/Scripts/DestroyParticles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticles : MonoBehaviour, IUpdate
+{
+    ParticleSystem particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateManager.updateManager.register(this);
+        this.particles = this.GetComponent<ParticleSystem>();
+    }
+
+    public void DoUpdate()
+    {
+        if (this.particles && !this.particles.IsAlive())
+        {
+            Destroy(this.gameObject);
+            UpdateManager.updateManager.unregister(this);
+        }
+    }
+}
