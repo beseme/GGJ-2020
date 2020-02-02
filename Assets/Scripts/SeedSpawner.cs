a7dc11f3fa425ef51d1b89fac6254c54c5c58c8e
Bennet Meier <bennet.meier@gmx.net>
2020-02-02 16:24:57 +0100

    Cumspawner

---
+++ Assets/Scripts/SeedSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using Random = UnityEngine.Random;
+
+public class SeedSpawner : MonoBehaviour
+{
+    [SerializeField] private CinemachineConfiner _confiner = null;
+    [SerializeField] private GameObject _seed = null;
+    [SerializeField] private float _waveCooldown = 0;
+    [SerializeField] private float _spawnAmount = 0;
+
+    private float _waveCD = 0;
+    private float _spawn = 0;
+
+    private void Awake()
+    {
+        _spawnAmount /= 60;
+        _waveCD = _waveCooldown;
+        _spawn = _spawnAmount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_spawnAmount >= 0)
+        {
+            int spawnDirection = Random.Range(1, 2);
+            float positiveOffest =
+                Random.Range(_confiner.m_BoundingVolume.bounds.min.y, _confiner.m_BoundingVolume.bounds.max.y);
+            float negativeOffest =
+                -Random.Range(_confiner.m_BoundingVolume.bounds.min.y, _confiner.m_BoundingVolume.bounds.max.y);
+            if (spawnDirection == 1)
+                Instantiate(_seed, (Vector2) _confiner.m_BoundingVolume.bounds.min + Vector2.up * positiveOffest,
+                    Quaternion.identity);
+            else if (spawnDirection == 1)
+                Instantiate(_seed, (Vector2) _confiner.m_BoundingVolume.bounds.max + Vector2.up * negativeOffest,
+                    Quaternion.identity);
+
+            _waveCD = _waveCooldown;
+        }
+        else if (_waveCD <= 0)
+        {
+            _spawn = _spawnAmount;
+        }
+
+        _spawn -= Time.deltaTime;
+        _waveCD -= Time.deltaTime;
+
+    }
+}
---
+++ Assets/Scripts/WhiteGoo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using Random = UnityEngine.Random;
+
+public class WhiteGoo : Actor
+{
+    private float _speedX = 0;
+    private float _speedY = 0;
+    
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        _speedX = Random.Range(1, 10);
+        _speedY = Random.Range(1, 10);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerController>())
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        this.MoveX(_speedX * Time.fixedDeltaTime);
+        this.MoveY(_speedY * Time.fixedDeltaTime);
+    }
+}
