0df0b9da6e892557a8b5efb7debe1bb2aa508543
Bennet Meier <bennet.meier@gmx.net>
2020-02-02 16:40:02 +0100

    go away buuuuugs

--- Assets/Scripts/WhiteGoo.cs
+++ Assets/Scripts/WhiteGoo.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 using UnityEngine.Experimental.PlayerLoop;
 using Random = UnityEngine.Random;
 
-public class WhiteGoo : Actor
+public class WhiteGoo : MonoBehaviour
 {
     private float _speedX = 0;
     private float _speedY = 0;
+    private Rigidbody2D _rig = null;
     
     // Start is called before the first frame update
-    new void Start()
+    void Start()
     {
-        base.Start();
-        _speedX = Random.Range(1, 10);
-        _speedY = Random.Range(1, 10);
+        _rig = GetComponent<Rigidbody2D>();
+        _speedX = Random.Range(-10f, 10);
+        _speedY = Random.Range(-10f, 10);
     }
     
     private void OnTriggerEnter2D(Collider2D other)
@@ -28,7 +29,6 @@ public class WhiteGoo : Actor
 
     private void FixedUpdate()
     {
-        this.MoveX(_speedX * Time.fixedDeltaTime);
-        this.MoveY(_speedY * Time.fixedDeltaTime);
+        _rig.velocity = new Vector2(_speedX, _speedY);
     }
 }
